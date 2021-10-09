2deec24a11ea80470839c2840bd86989e4a90c31
annisaptr19 <annisaputri0478@gmail.com>
2021-10-09 18:53:01 +0700

    Latihan Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan: Annisa</li>
+        <li>Nama Belakang: Putri Juliyanti</>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Danau Poso V No. 33 Tangerang</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Sepakbola</li>
+    </ul>
+</section>
+    
+
+
+
